bc556bc8c650fb21aa3368354933a715a1ad9967
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2018-10-20 23:46:59 +0600

    packages updated with electron starter

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,38 +1,33 @@
 const electron = require('electron');
-// Module to control application life.
+
 const app = electron.app;
-// Module to create native browser window.
+
 const BrowserWindow = electron.BrowserWindow;
 
 const path = require('path');
 const url = require('url');
+const fs = require('fs');
+const isDev = require("electron-is-dev");
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow() {
-    // Create the browser window.
+    //noinspection JSUnresolvedVariable
+
     mainWindow = new BrowserWindow({width: 800, height: 600});
 
-    // and load the index.html of the app.
-    mainWindow.loadURL('http://localhost:3000');
-
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    mainWindow.loadURL(
+        isDev
+            ? "http://localhost:3000"
+            : `file://${path.join(__dirname, "../build/index.html")}`
+    );
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
         mainWindow = null
     })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
